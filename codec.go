9de59bb1b048432364c7a88411b9eb35cbe83b20
kharism <greyroot00@gmail.com>
2019-03-21 14:39:13 +0700

    initial commit, only support string and map[string]string

---
+++ codec.go
@@ -0,0 +1,200 @@
+package solrjavacodec
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"reflect"
+
+	tk "github.com/eaciit/toolkit"
+
+	"github.com/eaciit/errorlib"
+)
+
+const (
+	//basic data type
+
+	NULL       = 0
+	BOOL_TRUE  = 1
+	BOOL_FALSE = 2
+	BYTE       = 3
+	SHORT      = 4
+	DOUBLE     = 5
+	INT        = 6
+	LONG       = 7
+	FLOAT      = 8
+	DATE       = 9
+	MAP        = 10
+	SOLRDOC    = 11
+	SOLRDOCLST = 12
+	BYTEARR    = 13
+	ITERATOR   = 14
+
+	STR           = byte(1 << 5)
+	SINT          = byte(2 << 5)
+	SLONG         = byte(3 << 5)
+	ARR           = byte(4 << 5) //
+	ORDERED_MAP   = byte(5 << 5) // SimpleOrderedMap (a NamedList subclass, and more common)
+	NAMED_LST     = byte(6 << 5) // NamedList
+	EXTERN_STRING = byte(7 << 5)
+)
+
+var packageName = "solrjavacodec"
+var modCursorDecode = "Decoder"
+var readStringAsCharSeq = false
+
+func UnmarshalByte(m interface{}, data []byte) error {
+	if !tk.IsPointer(m) {
+		return errorlib.Error(packageName, modCursorDecode, "Fetch", "Model object should be pointer")
+	}
+	buff := bytes.NewBuffer(data)
+	buff.ReadByte() //ambil versi, gak dipakai dulu
+	fmt.Println("LLLL")
+	return readVal(m, buff)
+}
+func UnmarshalStream(m interface{}, dataStream *bytes.Buffer) error {
+	e := readVal(m, dataStream)
+	if e == io.EOF {
+		return nil
+	} else {
+		return e
+	}
+}
+
+func readVal(m interface{}, reader *bytes.Buffer) error {
+	return readObject(m, reader)
+}
+func readObject(m interface{}, dis *bytes.Buffer) error {
+	tagByte, err := dis.ReadByte()
+	fmt.Printf("TAG %x\n", tagByte)
+	if err != nil {
+		return err
+	}
+	checkType := tagByte >> 5
+	switch checkType {
+	case STR >> 5:
+		err = readStr(dis, readStringAsCharSeq, &tagByte, m)
+		if err != nil {
+			return err
+		}
+	case EXTERN_STRING >> 5:
+		fmt.Println("Read ExternString")
+		err = readExternString(m, dis, &tagByte)
+	case ORDERED_MAP >> 5:
+		err = readOrderedMap(m, dis, tagByte)
+		return err
+	}
+	return nil
+}
+func readExternString(m interface{}, dis *bytes.Buffer, tagByte *byte) error {
+	idx, err := readSize(dis, *tagByte)
+	if err != nil {
+		return err
+	}
+	if idx != 0 {
+		//do something later
+	} else {
+		//only do this at the moment
+		//idx == 0 means it has a string value
+		*tagByte, err = dis.ReadByte()
+		if err != nil {
+			return err
+		}
+		readStr(dis, false, tagByte, m)
+	}
+	return nil
+}
+func readOrderedMap(m interface{}, dis *bytes.Buffer, tagByte byte) error {
+	fmt.Println("Read Ordered Map")
+	sz, err := readSize(dis, tagByte)
+	if err != nil {
+		return err
+	}
+	newMap := map[string]interface{}{}
+	for ii := 0; ii < sz; ii++ {
+		key1 := ""
+		err := readVal(&key1, dis)
+		if err != nil {
+			return err
+		}
+		//fmt.Println("FoundKey", key1)
+		var data interface{}
+		err = readVal(&data, dis)
+		if err != nil {
+			return err
+		}
+		//fmt.Println("FoundData", data)
+		newMap[key1] = data
+	}
+	*m.(*map[string]interface{}) = newMap
+	return nil
+}
+func readSize(dis *bytes.Buffer, tagByte byte) (int, error) {
+	var sz int
+	sz = int(tagByte) & 0x1f
+	fmt.Printf("ReadSize %x %x\n", sz, tagByte)
+	if sz == int(0x1f) {
+		fmt.Println("Ada tambahan")
+		szAddition, err := readVInt(dis, tagByte)
+		if err != nil {
+			return 0, err
+		}
+		sz += szAddition
+	}
+	fmt.Println("Total read size", sz)
+	return sz, nil
+}
+
+// this function is used to read any data that have length more than 255, maybe, not so sure
+// about that
+func readVInt(dis *bytes.Buffer, tagByte byte) (int, error) {
+	b, err := dis.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	fmt.Printf("Read VINT %x\n", b)
+	var i uint
+	i = uint(b) & 0x7F
+	var shift uint
+	for shift = 7; (b & 0x80) != 0; shift += 7 {
+		b, err = dis.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+		i |= (uint(b) & 0x7F) << uint(shift)
+	}
+	fmt.Println("result read VINT", i)
+	return int(i), nil
+}
+
+// need option to read as characte sequence, but for the moment force read as UTF8/UTF16 character
+func readUtf8(dis *bytes.Buffer) error {
+
+	return nil
+}
+func _readStr(dis *bytes.Buffer, sz int, m interface{}) error {
+	tempBuff := make([]byte, sz)
+	_, err := dis.Read(tempBuff)
+	if err != nil {
+		return err
+	}
+	newString := string(tempBuff)
+	//fmt.Println("newString", newString)
+	//*m.(*string) = newString
+	setValue(m, newString)
+	return nil
+}
+func setValue(m interface{}, value interface{}) {
+	//v := reflect.TypeOf(m).Elem().Elem()
+	//iv := reflect.New(v).Interface()
+	reflect.ValueOf(m).Elem().Set(reflect.ValueOf(value))
+}
+func readStr(dis *bytes.Buffer, readStringAsCharSeq bool, tagByte *byte, m interface{}) error {
+	//return readUtf8(dis)
+	fmt.Println("Read STR")
+	sz, err := readSize(dis, *tagByte)
+	if err != nil {
+		return err
+	}
+	return _readStr(dis, sz, m)
+}
---
+++ codec_test.go
@@ -0,0 +1,75 @@
+package solrjavacodec
+
+import (
+	"io/ioutil"
+	"strconv"
+	"testing"
+)
+
+func TestRead1(t *testing.T) {
+	bytes, _ := ioutil.ReadFile("select_javabin.bin")
+	//fd,_:=os.Open("select_javabin.bin")
+	data := map[string]interface{}{}
+	UnmarshalByte(&data, bytes)
+}
+func TestReadString(t *testing.T) {
+	inputBytes := []byte{0x02, 0xe0, 0x2e, 0x72, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x48, 0x65, 0x61, 0x64, 0x65, 0x72}
+	//reader := bytes.NewBuffer(inputBytes)
+	var kk string
+	kk = ""
+	err := UnmarshalByte(&kk, inputBytes)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	t.Log(kk)
+	if kk != "responseHeader" {
+		t.Fail()
+	}
+}
+func TestReadMapStringString(t *testing.T) {
+	inputBytes := []byte{0x02, 0xa2, 0xe0, 0x24, 0x6b, 0x65, 0x79, 0x31, 0x26, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x31, 0xe0, 0x24, 0x6b, 0x65, 0x79, 0x32, 0x26, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x32}
+	var kk map[string]interface{}
+	//kk = ""
+	err := UnmarshalByte(&kk, inputBytes)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	t.Log("Done Unmarshaling")
+	t.Log(kk)
+	keys := []string{"key1", "key2"}
+	values := []string{"value1", "value2"}
+	for i, k := range keys {
+		if _, ok := kk[k]; !ok {
+			t.Log("Key Not Found")
+			t.Fail()
+			return
+		}
+		if kk[k] != values[i] {
+			t.Log("Value Not same")
+			t.Fail()
+			return
+		}
+	}
+}
+func TestReadMapStringLongString(t *testing.T) {
+	inputString := "02a2e0246b6579312676616c756531e0246b6579323fbd0176616c7565327665656565727979796c6f6f6f6f6f6f6f6f6f6f6f6f6f6f6f6f6f6f6e6764616161616161616161747474747474746161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161"
+	inputBytes := []byte{}
+	for i := 0; i < len(inputString); i += 2 {
+		str := inputString[i : i+2]
+		uu, err := strconv.ParseInt(str, 16, 9)
+		if err != nil {
+			t.Log(err.Error())
+			return
+		}
+		//t.Logf("%s %02x", str, uu)
+		inputBytes = append(inputBytes, byte(uu))
+	}
+	var kk map[string]interface{}
+	//kk = ""
+	err := UnmarshalByte(&kk, inputBytes)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	t.Log("Done Unmarshaling")
+	t.Log(kk)
+}
